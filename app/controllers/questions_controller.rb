28dfb232dc5827ed36e89aaf4d97f5b31853527d
fumiya-yoshii <135097414+fumiya-yoshii@users.noreply.github.com>
2023-06-26 11:55:18 +0900

    回答投稿画面に質問内容の表示機能を実装する

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,22 +4,22 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
   def new
     @question = Question.new
   end
-  
+
   def create
     Question.create(question_params)
     redirect_to action: :index
-  end
-
-  def show
-    @question = Question.find(params[:id])
   end
 
   private
   def question_params
     params.require(:question).permit(:title, :content, :name)
   end
-  
+
 end
